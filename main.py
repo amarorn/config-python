e8aab5a5fe8d8f8b74f6b2e35841c926526931a0
Jose Amaro <joseamaro@Mac-Studio-de-Jose.local>
2022-12-01 15:00:21 -0300

    config: commit init

---
+++ main.py
@@ -0,0 +1,18 @@
+""" apresenta o resultado da soma de dois números """
+
+
+def soma(num_1, num_2):
+    """retorna a soma de dois números"""
+    soma_num = num_1 + num_2
+    return soma_num
+
+
+def minha_funcao(
+    argumernto_muito_grande,
+    argumento_muito_grande2,
+    argumento_muito_grande3,
+    argumento_muito_grande4,
+    argumento_muito_grande5,
+):
+    print("O resultado da soma é: ", argumernto_muito_grande)
+    return True
---
+++ main_test.py
@@ -0,0 +1,6 @@
+from main import soma
+
+
+def test_soma() -> None:
+    """Testa a função soma"""
+    assert soma(1, 2) == 3
